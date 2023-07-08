4b1d1b92acf032c4377cc86293fb19eea65a5853
tanveer.hussain-ext <ahmad.shakeel7870@gmail.com>
2023-07-08 18:04:23 +0400

    First commit setting app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ConverterComponent } from './converter/converter.component';
+import { DetailsComponent } from './details/details.component';
+import { ChartComponent } from './chart/chart.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConverterComponent,
+    DetailsComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
