ea2d5cc464bac495ac879e2358265454333a1103
Coco7870 <you@example.com>
2023-07-08 18:41:46 +0400

    Adding currecnies list

---
+++ src/app/currencies.ts
@@ -0,0 +1,25 @@
+export class Currencies {
+    static currencies = [
+      { code: "USD", name: "United States Dollar" },
+      { code: "GBP", name: "British Pound Sterling" },
+      { code: "EUR", name: "Euro" },
+      { code: "JPY", name: "Japanese Yen" },
+      { code: "AUD", name: "Australian Dollar" },
+      { code: "CAD", name: "Canadian Dollar" },
+      { code: "CHF", name: "Swiss Franc" },
+      { code: "CNY", name: "Chinese Yuan" },
+      { code: "NZD", name: "New Zealand Dollar" },
+      { code: "SEK", name: "Swedish Krona" },
+      { code: "INR", name: "Indian Rupee" },
+      { code: "SGD", name: "Singapore Dollar" },
+      { code: "HKD", name: "Hong Kong Dollar" },
+      { code: "MXN", name: "Mexican Peso" },
+      { code: "ZAR", name: "South African Rand" },
+      { code: "BRL", name: "Brazilian Real" },
+      { code: "RUB", name: "Russian Ruble" },
+      { code: "TRY", name: "Turkish Lira" },
+      { code: "KRW", name: "South Korean Won" },
+      { code: "NOK", name: "Norwegian Krone" },
+    ];
+  }
+  
